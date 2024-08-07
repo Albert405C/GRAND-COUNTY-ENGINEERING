a277d8793f9bcc9c0269d7a7fd879ed42694c5fe
Albert405C <chrismitto9@gmail.com>
2024-08-07 11:14:31 +0300

    Updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,21 +4,15 @@ import './Hero.css';
 function Hero() {
     console.log("Hero rendered successfully");
 
-   
-
     return (
-        
         <section id="home" className="hero text-center">
-           <video autoPlay loop muted className="background-video">
-                <source src="Videos\Construction Commercial Cinematic Video - Trim - Trim.mp4" type="video/mp4" />
+            <video autoPlay loop muted className="background-video">
+                <source src={`${process.env.PUBLIC_URL}/Videos/Construction Commercial Cinematic Video - Trim - Trim.mp4`} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="container">
-                
                 <h1 className="display-4 mb-4">Engineering Excellence.</h1>
                 <p className="lead mb-5">Comprehensive solutions for a sustainable future.</p>
-                
-               
             </div>
         </section>
     );
